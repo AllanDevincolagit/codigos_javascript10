200cc4ac068d3b787917eb4a76f30babd787d67c
AllanDevincolagit <profissional.allandevincola@gmail.com>
2021-07-13 11:19:37 -0300

    primeiro codigo dnv

---
+++ main.js
@@ -0,0 +1,12 @@
+alert("Oi dnv")
+var nome = "Allan" //String tem que ter aspas!!!
+var idade = 19 // Se transformar o número em string usando "" asoperações ficarão indisponiveis
+var idade2 = 1
+var frase = "O japão é o pais com o maior número de titulos de copa do mundo"
+//alert(nome + " você já tem " + idade + " caramba!!!") // Concatenação no javascript é com "+"
+alert(nome.replace("Allan", "Aline"))
+alert(idade + idade2)
+console.log(nome)
+console.log(idade+idade2)
+console.log(frase.replace("japão", "Brasil"))
+console.log(frase.toUpperCase())
