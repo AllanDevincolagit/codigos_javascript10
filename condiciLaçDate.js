ea5dd2118cd3bd9f7fde8361c645c4a65b84207c
AllanDevincolagit <79774220+AllanDevincolagit@users.noreply.github.com>
2021-07-14 16:16:17 -0300

    continuação estudo js
    Laços de repetição, estrutura condicional e date

---
+++ condiciLaçDate.js
@@ -0,0 +1,25 @@
+//var idade = 18
+/*var idade = prompt("Qual é a sua idade ?")
+if (idade >= 18){
+    alert("Maior de idade")
+}
+
+else{
+    alert("Menor de idade")
+}*/
+
+/*var count = 0
+while (count = 0){
+    console.log(count)
+    count = count ++
+}*/
+
+/*for(count = 0; count<=5; count++){
+    alert(count)
+}*/
+
+/*var data = new Date()  //pega a data, hora, mês, ano tudo...
+alert(data)*/
+
+var data = new Date()
+alert(data.getFullYear()) //Sempre colocar um ponto quanto concatenar um comando com outro comando e abrir parênteses extras
